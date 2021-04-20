3fa568dee2de9ea6c7fbb6783573a2582d0fcf3c
ChadH28 <chadhoosain@gmail.com>
2021-04-20 16:19:13 +0200

    Worked on radio buttons

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -9,17 +9,38 @@
     <label>Password:</label>
     <input type="password" v-model="password" required />
 
-    <label>Role:</label><br>
+    <div>
+      <input v-model="traits" type="checkbox" value="Analytical" required />
+      <label>Analytical</label>
+    </div>
+
+    <div>
+      <input v-model="traits" type="checkbox" value="Book-worm" required />
+      <label>Book-worm</label>
+    </div>
+
+    <div>
+      <input v-model="traits" type="checkbox" value="Workhorse" required />
+      <label>Workhorse</label>
+    </div>
+
+    <label>Role:</label><br />
     <select v-model="role">
-        <option value="developer"> Web Developer</option>
-        <option value="designer"> Web Designer</option>
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input v-model="terms" type="checkbox" required />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
   <p>
-      User: {{name}} <br>
-      Email: {{email}} <br>
-      Password: {{password}} <br>
-      Role: {{role}} <br>
+    User: {{ name }} <br />
+    Email: {{ email }} <br />
+    Password: {{ password }} <br />
+    Role: {{ role }} <br />
+    Terms: {{ terms }} <br />
   </p>
 </template>
 
@@ -27,10 +48,12 @@
 export default {
   data() {
     return {
-      email: '',
-      password: '',
-      name: '',
-      role: ''
+      email: "",
+      password: "",
+      name: "",
+      role: "",
+      terms: false,
+      traits: []
     };
   },
 };
@@ -39,8 +62,10 @@ export default {
 <style>
 form {
   max-width: 450px;
+  width: 400px;
   margin: 30px auto;
   background: white;
+  box-sizing: border-box;
   text-align: left;
   padding: 40px;
   border-radius: 12px;
@@ -66,5 +91,13 @@ options {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
