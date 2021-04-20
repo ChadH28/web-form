9543f46a56fd263c9ad4c5cccc5f5a00abac4f67
ChadH28 <chadhoosain@gmail.com>
2021-04-20 14:27:56 +0200

    Worked with databinding and v-models

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,45 +1,61 @@
 <template>
   <form>
-      <label>Email:</label>
-      <input type='email' required>
+    <label>User:</label>
+    <input type="text" v-model="name" required />
+
+    <label>Email:</label>
+    <input type="email" v-model="email" required />
+
+    <label>Password:</label>
+    <input type="password" v-model="password" required />
   </form>
+  <p>
+      User: {{name}} <br>
+      Email: {{email}} <br>
+      Password: {{password}} 
+      
+  </p>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      email: '',
+      password: '',
+      name: ''
+    };
+  },
+};
 </script>
 
 <style>
-
-form{
-    max-width: 420px;
-    margin: 30px auto;
-    background: white;
-    text-align: left;
-    padding: 40px;
-    border-radius: 12px;
+form {
+  max-width: 450px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 12px;
 }
 
-label{
-    color: #aaa;
-    display: inline-block;
-    margin: 25px 0 15px;
-    font-size: 0.6em;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: bold;
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
 }
 
-input{
-    display: block;
-    padding: 10px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: 1px solid #ddd;
-    color: #555
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
 }
-
 </style>
